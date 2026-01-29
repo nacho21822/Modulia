3f3b00d32d3cc62d3eda70b8425cf975b61b13b8
Fernando Serer <you@example.com>
2026-01-29 12:42:10 +0100

    Resolver

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="../css/login-modal.css" />
+    <link rel="stylesheet" href="{{asset('css/login-modal.css') }}" />
   </head>
   <body>
     <div class="container" id="container">
